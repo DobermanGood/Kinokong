4aaac2584b7fbdacd75ddcc85314d40626a2afed
mail.dobermangood@gmail.com <mail.dobermangood@gmail.com>
2019-07-15 03:44:17 +1200

    test page added

---
+++ pages/testpage.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>
+      Test page for Git testing
+    </h1>
+</template>
+
+<script>
+    export default {
+        name: "testpage"
+    }
+</script>
+
+<style scoped>
+
+</style>
